3a7c4a7f350ce267547975c3df79109f41392fcc
Akbarinfo <girtbezbet@gmail.com>
2022-08-03 10:23:34 +0500

    updated config

--- src/components/Aside/Header/index.jsx
+++ src/components/Aside/Header/index.jsx
@@ -1,26 +1,47 @@
-import i18next from "i18next";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import GET from "../../../API/GET";
 
 // style
 import './asideHeader.scss'
 
 // images
-import facebook from '../../../assets/icons/icons8-facebook.svg'
-import twitter from '../../../assets/icons/icons8-twitter-circled.svg'
-import instagram from '../../../assets/icons/icons8-instagram.svg'
-import youtube from '../../../assets/icons/icons8-youtube.svg'
-import telegram from '../../../assets/icons/icons8-telegram-app.svg'
+import facebooks from '../../../assets/icons/icons8-facebook.svg'
+import twitters from '../../../assets/icons/icons8-twitter-circled.svg'
+import instagrams from '../../../assets/icons/icons8-instagram.svg'
+import youtubes from '../../../assets/icons/icons8-youtube.svg'
+import telegrams from '../../../assets/icons/icons8-telegram-app.svg'
 
 export default function AsideHeader() {
 
+  let {id} = useParams()
+  let [data, setData] = useState()
+
+  const newItem = async () => {
+    try {
+      const response = await GET.config();
+      setData(response.data)
+
+    } catch (err) {
+      console.log(err)
+      return
+    }
+  }
+
+  useEffect(() => {
+    newItem()
+  }, [])
+
+  console.log(data)
+
   return (
-    <>  
+    <>
       <ul className="aside__blist">
-        <li className="aside__bitem"><a href="http://" target="_blank"><img src={facebook} alt="facebook" width="40" height="40" /></a></li>
-        <li className="aside__bitem"><a href="http://" target="_blank"><img src={twitter} alt="twitter" width="40" height="40" /></a></li>
-        <li className="aside__bitem"><a href="http://" target="_blank"><img src={instagram} alt="intagram" width="40" height="40" /></a></li>
-        <li className="aside__bitem"><a href="http://" target="_blank"><img src={youtube} alt="youtube" width="40" height="40" /></a></li>
-        <li className="aside__bitem"><a href="http://" target="_blank"><img src={telegram} alt="telegram" width="40" height="40" /></a></li>
+        <li className="aside__bitem"><a href="http://" target="_blank"><img src={facebooks} alt="facebook" width="40" height="40" /></a></li>
+        <li className="aside__bitem"><a href="" target="_blank"><img src={twitters} alt="twitter" width="40" height="40" /></a></li>
+        <li className="aside__bitem"><a href="http://" target="_blank"><img src={instagrams} alt="intagram" width="40" height="40" /></a></li>
+        <li className="aside__bitem"><a href="http://" target="_blank"><img src={youtubes} alt="youtube" width="40" height="40" /></a></li>
+        <li className="aside__bitem"><a href="http://" target="_blank"><img src={telegrams} alt="telegram" width="40" height="40" /></a></li>
 
       </ul>
     </>
